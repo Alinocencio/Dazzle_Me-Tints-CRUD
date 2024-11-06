9b849887db9c7d6b18b9093709a2f464ab74c478
Alinocencio <alyssainocencio2315@gmail.com>
2024-11-06 15:28:33 +0800

    Add files via upload

---
+++ connections/connection_customer.php
@@ -0,0 +1,28 @@
+<?php 
+function connection_customer(){
+//variable declarations 
+$host = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "OlShop_DMT"; /*database name here*/
+//assigning of instance to mysqli method 
+$con = new mysqli($host,$username,$password,$database);
+
+if($con-> connect_error){
+    echo $con ->connect_error;
+    }else{
+    return $con;
+    }
+
+    try {
+        $con = new mysqli($host, $username, $password, $database);
+        $con->set_charset("utf8mb4");
+        return $con;
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        // Display a generic error message to the user instead of echoing the error message
+        die("Oops! Something went wrong. Please try again later.");
+    }
+
+}
+?>
